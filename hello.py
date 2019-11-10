0834da66f9ee49afa2acb575cbe8e6d355ea683a
WinnerOK <danman339@gmail.com>
2019-11-10 11:07:54 +0300

    credentials

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import re
+from os import environ
+
 app = Flask(__name__)
+
+pattern = re.compile(r"^postgres:\/\/(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>\d+)\/(?P<database>.+)$")
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    url = environ["DATABASE_URL"]
+    _, user, password,  host, port, database, __ = pattern.split(url)
+    return f'host: {host}\nport: {port}\ndatabase: {database}\nuser: {user}\npassword: {password}'
 
 if __name__ == '__main__':
     app.run()
