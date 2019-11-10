960d38318ef8d4d69a7000ce3c06e9fd95a837a3
WinnerOK <danman339@gmail.com>
2019-11-10 11:58:55 +0300

    for sake of easy pg parsing

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ pattern = re.compile(r"^postgres:\/\/(?P<user>.+):(?P<password>.+)@(?P<host>.+):
 def hello_world():
     url = environ["DATABASE_URL"]
     _, user, password,  host, port, database, __ = pattern.split(url)
-    return f'host: {host}<br>port: {port}<br>database: {database}<br>user: {user}<br>password: {password}'
+    return f'host: {host}<br>port: {port}<br>dbname: {database}<br>user: {user}<br>password: {password}'
 
 if __name__ == '__main__':
     app.run()
