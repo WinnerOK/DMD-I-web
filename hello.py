315512cbcd31ac92eb781174b51cb33d8c51074e
WinnerOK <danman339@gmail.com>
2019-11-17 13:22:22 +0300

    view templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import re
 from os import environ
 
@@ -7,8 +7,13 @@ app = Flask(__name__)
 pattern = re.compile(r"^postgres:\/\/(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>\d+)\/(?P<database>.+)$")
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', name="HFDSGSDKLFHHJK")
+
+
 @app.route('/cred/postgres')
-def hello_world():
+def postgres_cred():
     url = environ["DATABASE_URL"]
     _, user, password, host, port, database, __ = pattern.split(url)
     return f'host: {host}<br>port: {port}<br>dbname: {database}<br>user: {user}<br>password: {password}'
