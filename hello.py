ceaaf0468ebae7866489dafae35fc106ceecb741
WinnerOK <danman339@gmail.com>
2019-11-17 13:11:01 +0300

    updated cred url and requirements

--- hello.py
+++ hello.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 pattern = re.compile(r"^postgres:\/\/(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>\d+)\/(?P<database>.+)$")
 
-@app.route('/')
+
+@app.route('/cred/postgres')
 def hello_world():
     url = environ["DATABASE_URL"]
-    _, user, password,  host, port, database, __ = pattern.split(url)
+    _, user, password, host, port, database, __ = pattern.split(url)
     return f'host: {host}<br>port: {port}<br>dbname: {database}<br>user: {user}<br>password: {password}'
+
 
 if __name__ == '__main__':
     app.run()
