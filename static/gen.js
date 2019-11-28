9cba3966479702960015cb6fbb8c94d5dfc8684c
VanishMax <maxkors2014@gmail.com>
2019-11-28 23:08:47 +0300

    Add progress for generating data

--- static/gen.js
+++ static/gen.js
@@ -45,19 +45,45 @@ const ids = [{
   field: 'notice_board_messages',
 }];
 
+let interval = null;
+let progress = 0;
+const startLoad = (e) => {
+  if (!e || parseInt(e.target.getAttribute('data-tab')) !== 6) {
+    progress = 0;
+    document.getElementById('execute').disabled = true;
+    let up = true;
+    interval = setInterval(() => {
+      if (up) {
+        if (progress >= 100) up = false;
+        else progress += 10;
+      } else {
+        if (progress <= 0) up = true;
+        else progress -= 10;
+      }
+      document.getElementById('progress').style.width = progress + '%';
+    }, 100);
+  }
+};
+
+document.getElementById('execute').addEventListener('click', startLoad);
 document.getElementById('execute').addEventListener('click', function () {
   let data = {};
   for (let obj of ids) data[obj.field] = parseInt(document.getElementById(obj.id).value);
   data['execute_queries'] = document.getElementById('exe_queries').checked;
   data['truncate_tables'] = document.getElementById('trunc').checked;
 
+  console.log('kek');
   let xhr = new XMLHttpRequest();
   xhr.open('POST', '/populate', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
       window.location.href = this.response;
+      clearInterval(interval);
+      progress = 0;
+      document.getElementById('progress').style.width = progress + '%';
     }
   };
   xhr.send(JSON.stringify(data));
 });
+
